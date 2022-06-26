600ea88cb56af27422a327d73b501dfc88bac1fe
NyirőTeofil <portrider01234@gmail.com>
2022-06-26 22:35:15 +0200

    I confused the .ToLower() from C# with .toLowerCase()

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function computerPlay() {
 
 function playRound(playerChoice, computerChoice) {
     /*Making user input case insensitive*/ 
-    let /*Case Insesitive playerChice*/ CIP = playerChoice.ToLower(); 
+    let /*Case Insesitive playerChice*/ CIP = playerChoice.toLowerCase(); 
 
     if (CIP === "rock" && computerChoice === "scissor" || CIP === "paper" && computerChoice === "rock"|| CIP === "scissor" && computerChoice === "paper") {
         return `You win! ${playerChoice} beats ${computerChoice}`;
