b46beb77c464a1913df88a3c7ef36744cb4b99bc
NyirőTeofil <portrider01234@gmail.com>
2022-06-26 22:14:34 +0200

    The game looks complete for now...

--- game.js
+++ game.js
@@ -10,8 +10,32 @@ function playRound(playerChoice, computerChoice) {
 
     if (CIP === "rock" && computerChoice === "scissor" || CIP === "paper" && computerChoice === "rock"|| CIP === "scissor" && computerChoice === "paper") {
         return `You win! ${playerChoice} beats ${computerChoice}`;
+        playerScore++;
     } else {
-        return `You lost! ${computerChoice} beats ${playerChoice}`;
+        return `You lost! ${computerChoice} beats ${playerChoice}\n Round: ${playerScore + compScore}\nYour score: ${playerScore} : Enemy's score: ${compScore}`;
+        compScore++;
     }
+}
+
+function game() {
+const playerScore = 0;
+const compScore =  0;
+const winner = "";
+
+console.log(`Round: ${playerScore + compScore}\nYour score: ${playerScore} : Enemy's score: ${compScore}`)
+
+while (playerScore < 3 && compScore < 3) {
+    let playerChoice = prompt("What is your weapos of choice? :", "rock");
+
+    playRound(playerChoice, computerPlay);
+}
+
+if (playerScore === 3) {
+    winner = "you";
+} else {
+    winner = "the computer";
+}
+
+console.log(`${winner} won the game!`);
 }
 
